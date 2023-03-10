d3a48a9854cadaf3159575f4996d7c3a17b9faa4
Tobshub <tobanigeorge@gmail.com>
2023-03-10 03:23:44 +0100

    remove "spam" log

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,9 +1,6 @@
 import { Server } from "socket.io";
-import LOG from "./config/log";
 import registerUserHandlers from "./api/user/handler";
 import registerRoomHandlers from "./api/room/handler";
-
-const rooms: { room_id: string; name: string }[] = [];
 
 // FIXIT: wrap message type
 // export interface ClientToServerEvents {
@@ -12,8 +9,6 @@ const rooms: { room_id: string; name: string }[] = [];
 
 export default function socketHandler(io: Server) {
   io.on("connection", (socket) => {
-    LOG("info", "Established Socket Connection", socket.id);
-
     registerUserHandlers(io, socket);
     registerRoomHandlers(io, socket);
   });
