4b354e2560477fdbc4c35935ab1ef131f9e957fe
Tobshub <tobanigeorge@gmail.com>
2023-03-13 19:26:26 +0100

    fix: getting friend list

--- server/src/api/user/controller/user/get-user.ts
+++ server/src/api/user/controller/user/get-user.ts
@@ -12,6 +12,7 @@ export async function getUserPrivate(id: string) {
         username: true,
         publicId: true,
         friendsWith: { select: { username: true, publicId: true } },
+        friendsOf: { select: { username: true, publicId: true } },
         receivedFriendRequests: {
           select: { id: true, status: true, sender: { select: { publicId: true, username: true } } },
         },
@@ -22,7 +23,12 @@ export async function getUserPrivate(id: string) {
       return Err("user not found");
     }
 
-    return Ok({ email: user.email, username: user.email, publicId: user.publicId, friends: user.friendsWith });
+    return Ok({
+      email: user.email,
+      username: user.email,
+      publicId: user.publicId,
+      friends: user.friendsWith.concat(user.friendsOf),
+    });
   } catch (err) {
     LOG.error(err, "Error: failed to get username");
     return Err("an error occured", err);
