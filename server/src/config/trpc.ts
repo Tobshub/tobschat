98bab6323e5c8e30f6a6d1719d8d48441b85c405
Tobshub <tobanigeorge@gmail.com>
2023-03-25 14:25:28 +0100

    code clean up

--- server/src/config/trpc.ts
+++ server/src/config/trpc.ts
@@ -17,18 +17,18 @@ export const tRouter = t.router;
 export const tProcedure = t.procedure;
 export const tError = TRPCError;
 
-export const authedProcedure = t.procedure.use(
-  t.middleware(async ({ ctx, next }) => {
-    if (!ctx.auth) {
-      Log.error(["Tried to access Authed Procedure without a token"]);
-      throw new tError({ code: "BAD_REQUEST", message: "user token is missing" });
-    }
-    const validate = appToken.validate(ctx.auth);
-    if (!validate.ok) {
-      Log.error(["Failed to Validate token", ctx.auth]);
-      throw new tError({ code: "UNAUTHORIZED", message: "failed to validate token" });
-    }
-    return next({ ctx: { auth: undefined, id: validate.value.id } });
-  })
-);
+const authMiddleware = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.auth) {
+    Log.error(["Tried to access Authed Procedure without a token"]);
+    throw new tError({ code: "BAD_REQUEST", message: "user token is missing" });
+  }
+  const validate = appToken.validate(ctx.auth);
+  if (!validate.ok) {
+    Log.error(["Failed to Validate token", ctx.auth]);
+    throw new tError({ code: "UNAUTHORIZED", message: "failed to validate token" });
+  }
+  return next({ ctx: { auth: undefined, id: validate.value.id } });
+});
+
+export const authedProcedure = t.procedure.use(authMiddleware);
 
