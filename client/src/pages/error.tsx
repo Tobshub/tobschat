ac686f6c1330459b5b5fc8d76b434df8e4a8bb2b
Tobshub <tobanigeorge@gmail.com>
2023-03-29 21:36:29 +0100

    text error

--- client/src/pages/error.tsx
+++ client/src/pages/error.tsx
@@ -4,13 +4,13 @@ export function ErrorPage() {
   const error = useRouteError() as Error;
   return (
     <div>
-      <h1>Oops! Something bad happenedwent wrong</h1>
+      <h1>Oops! Something bad happened</h1>
       <small>{"message" in error? error.message : null}</small>
       <p>
         Don't panic. One of these solutions should work:
         <ul>
           <li><Link to="/">Go to home</Link></li>
-          <li><button className="btn btn-link p-0" onClick={window.location.reload}>Reload the page</button></li>
+          <li><button className="btn btn-link p-0" onClick={() => window.location.reload()}>Reload the page</button></li>
         </ul>
       </p>
     </div>
