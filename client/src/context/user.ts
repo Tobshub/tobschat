e44726f09a28d3fc09ce64d0af002c4ea3d8be9a
Tobshub <tobanigeorge@gmail.com>
2023-04-07 07:08:13 +0100

    removed unused file

--- client/src/context/user.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext<{ setContext(key: string, value: string): void; [key: string]: any }>({
-  setContext: (key, value) => null,
-  username: "",
-});
-export default UserContext;
-
