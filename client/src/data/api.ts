f40fa0e02b52d17908e7457737282202080f4407
Tobshub <tobanigeorge@gmail.com>
2023-05-10 20:32:06 +0100

    include image server url

--- client/src/data/api.ts
+++ client/src/data/api.ts
@@ -1,2 +1,3 @@
-export const server_url = import.meta.env.PROD ? "https://tobschat-api.onrender.com" : `http://${window.location.hostname}:8008`;
+export const server_url = import.meta.env.PROD ? "https://tobschat-api.onrender.com" : `http://${window.location.hostname}:4000`;
+export const img_server_url = import.meta.env.PROD ? "" : `http://${window.location.hostname}:4001`;
 
